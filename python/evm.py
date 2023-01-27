032da6aa73fe4f88dd8ef9541755271b1e3dd58b
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-01-27 00:51:04 -0800

    Add common code for PUSH opcodes
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- python/evm.py
+++ python/evm.py
@@ -33,34 +33,58 @@ class Context:
         self.code = code
         self.pc = pc
 
-def opcodeStop(ctx):
+def opcodeStop(ctx, dummy):
     return
 
-def opcodePush1(ctx):
-    ctx.stack.push(ctx.code[ctx.pc])
-    ctx.pc +=1
-
-
-def opcodePush2(ctx):
-    ctx.stack.push(ctx.code[ctx.pc])
-    ctx.pc +=1
-    ctx.stack.push(ctx.code[ctx.pc])
-    ctx.pc +=1
-
+def opcodePush(ctx, pushBytes):
+    for _ in range(pushBytes):
+        ctx.stack.push(ctx.code[ctx.pc])
+        ctx.pc +=1
 
 class OpcodeData:
-    def __init__(self, opcode, name, run):
+    def __init__(self, opcode, name, run, pushBytes=0):
         self.opcode = opcode
         self.name = name
         # function pointer
         self.run = run
+        # if push opcode, pushBytes represents number of
+        # bytes to be pushed into stack.
+        self.pushBytes = pushBytes
 
 opcode = {}
 opcode[0x00] = OpcodeData(0x00, "STOP", opcodeStop)
-opcode[0x60] = OpcodeData(0x60, "PUSH1", opcodePush1)
-opcode[0x61] = OpcodeData(0x61, "PUSH2", opcodePush2)
-
-
+opcode[0x60] = OpcodeData(0x60, "PUSH1", opcodePush, 1)
+opcode[0x61] = OpcodeData(0x61, "PUSH2", opcodePush, 2)
+opcode[0x62] = OpcodeData(0x62, "PUSH3", opcodePush, 3)
+opcode[0x63] = OpcodeData(0x63, "PUSH4", opcodePush, 4)
+opcode[0x64] = OpcodeData(0x64, "PUSH5", opcodePush, 5)
+opcode[0x65] = OpcodeData(0x65, "PUSH6", opcodePush, 6)
+opcode[0x66] = OpcodeData(0x66, "PUSH7", opcodePush, 7)
+opcode[0x67] = OpcodeData(0x67, "PUSH8", opcodePush, 8)
+opcode[0x68] = OpcodeData(0x68, "PUSH9", opcodePush, 9)
+opcode[0x69] = OpcodeData(0x69, "PUSH10", opcodePush, 10)
+opcode[0x6A] = OpcodeData(0x6A, "PUSH11", opcodePush, 11)
+opcode[0x6B] = OpcodeData(0x6B, "PUSH12", opcodePush, 12)
+opcode[0x6C] = OpcodeData(0x6C, "PUSH13", opcodePush, 13)
+opcode[0x6D] = OpcodeData(0x6D, "PUSH14", opcodePush, 14)
+opcode[0x6E] = OpcodeData(0x6E, "PUSH15", opcodePush, 15)
+opcode[0x6F] = OpcodeData(0x6F, "PUSH16", opcodePush, 16)
+opcode[0x70] = OpcodeData(0x70, "PUSH17", opcodePush, 17)
+opcode[0x71] = OpcodeData(0x71, "PUSH18", opcodePush, 18)
+opcode[0x72] = OpcodeData(0x72, "PUSH19", opcodePush, 19)
+opcode[0x73] = OpcodeData(0x73, "PUSH20", opcodePush, 20)
+opcode[0x74] = OpcodeData(0x74, "PUSH21", opcodePush, 21)
+opcode[0x75] = OpcodeData(0x75, "PUSH22", opcodePush, 22)
+opcode[0x76] = OpcodeData(0x76, "PUSH23", opcodePush, 23)
+opcode[0x77] = OpcodeData(0x77, "PUSH24", opcodePush, 24)
+opcode[0x78] = OpcodeData(0x78, "PUSH25", opcodePush, 25)
+opcode[0x79] = OpcodeData(0x79, "PUSH26", opcodePush, 26)
+opcode[0x7A] = OpcodeData(0x7A, "PUSH27", opcodePush, 27)
+opcode[0x7B] = OpcodeData(0x7B, "PUSH28", opcodePush, 28)
+opcode[0x7C] = OpcodeData(0x7C, "PUSH29", opcodePush, 29)
+opcode[0x7D] = OpcodeData(0x7D, "PUSH30", opcodePush, 30)
+opcode[0x7E] = OpcodeData(0x7E, "PUSH31", opcodePush, 31)
+opcode[0x7F] = OpcodeData(0x7F, "PUSH32", opcodePush, 32)
 
 def prehook(opcodeObj):
     print(f'Running opcode {hex(opcodeObj.opcode)} {opcodeObj.name}')
@@ -77,7 +101,7 @@ def evm(code):
         opcodeObj = opcode.get(op)
         if opcodeObj:
             prehook(opcodeObj)
-            opcodeObj.run(ctx)
+            opcodeObj.run(ctx, opcodeObj.pushBytes)
         else:
             print("Opcode implementation not found for ", hex(op))
             # return fake success but empty stack so that test case
